a53ab40eb7a1175b3275fa6b2d57f785aadaba96
erfan ghaffari <erfanghaffarie@gmail.com>
2020-04-26 19:34:26 +0430

    Update unfollow-non-followers.py
    update unfollow script

--- unfollow-non-followers.py
+++ unfollow-non-followers.py
@@ -1,1 +1,11 @@
+import os 
+import time
+import random
+from instabot import Bot
 
+bot = Bot(max_unfollows_per_day=100)
+bot.login(
+    username=os.getenv("INSTAGRAM_USERNAME"), 
+    password=os.getenv("INSTAGRAM_PASSWORD"),
+)
+bot.unfollow_non_followers()
